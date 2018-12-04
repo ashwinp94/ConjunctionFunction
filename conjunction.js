76202f604dcb330d87dca0204ea50048d9b95483
Ashwin Prakash <ashp94@gmail.com>
2018-12-04 10:51:35 -0600

    Corrected the code(i think)

---
+++ conjunction.js
@@ -0,0 +1,15 @@
+// Original Code that Needed to be Corrected
+// const conjunction = function (firstWord, secondWord) {
+//     const conjoinedWord = `${firstWord} ${secondWord}`
+// }
+
+// conjunction("Master", "Card")
+// console.log(conjoinedWord)
+
+
+let conjunction = function (firstWord, secondWord) {
+    const conjoinedWord = (`${firstWord} ${secondWord}`)
+    console.log(conjoinedWord);
+}
+
+conjunction("Master", "Card")
